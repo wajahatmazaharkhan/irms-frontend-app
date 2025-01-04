dcd5e05f72cff467069099e3a908bd730ad8b6ca
BHASKAR <bassnaidu1242@gmail.com>
2025-01-04 19:38:18 +0530

    undo the changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,4 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { useAuthContext } from "./context/AuthContext";
-import { NotFound } from "./Components/Notfound";
-import Navbar from "./Pages/Navbar";
-import Hero from "./Components/Hero";
-import Features from "./Components/Features";
-import TechStack from "./Components/TechStack";
-import Stats from "./Components/Stats";
-import Testimonials from "./Components/Testimonials";
-import FloatingIcons from "./Components/FloatingIcons";
-import ScrollingLogos from "./Components/ScrollingLogos";
-import SuccessStories from "./Components/SuccessStories";
-import CompanyMetrics from "./Components/CompanyMetrics";
-import GradientBackground from "./Components/GradientBackground";
-
-// Pages
+import { Routes, Route, useActionData } from "react-router-dom";
 import {
   Aboutus,
   FAQ,
@@ -43,16 +27,20 @@ import {
   AdminHelp,
   InternTasksSubmissions,
   Internleaveapplication,
+  IntroPage,
   AdminNotify,
 } from "./Pages/pageIndex";
-
-// Components
-import { AdminTask, CoreDashboard, Footer, Navbar as AppNavbar } from "./Components/compIndex";
-
-// Admin Components
+import {
+  AdminTask,
+  CoreDashboard,
+  Footer,
+  Navbar,
+} from "./Components/compIndex";
+import "./App.css";
+import { NotFound } from "./Components/Notfound";
+import { useAuthContext } from "./context/AuthContext";
 import AllAttendance from "./Admin/AllAttendance";
 
-// Route Protection Components
 const AdminRoute = ({ children }) => {
   const isAdmin = localStorage.getItem("isAdmin") === "true";
   return isAdmin ? children : <NotAuthorized />;
@@ -60,188 +48,217 @@ const AdminRoute = ({ children }) => {
 
 const PrivateRoute = ({ children }) => {
   const { loggedIn } = useAuthContext();
-  return loggedIn ? children : <Signin />;
+  return loggedIn ? children : <IntroPage />;
 };
 
 const App = () => {
   return (
-    <div className="min-h-screen relative overflow-hidden">
-      {/* Background and Floating Elements */}
-      <GradientBackground />
-      <FloatingIcons />
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/login" element={<Signin />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/reset-account-password" element={<ResetPassword />} />
+      <Route path="/aboutus" element={<Aboutus />} />
+      <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+      <Route path="/frequently-asked-questions" element={<FAQ />} />
+      <Route path="*" element={<NotFound />} />
 
-      <Routes>
-        {/* Public Landing Page */}
-        <Route
-          path="/"
-          element={
-            <>
-              <Navbar />
-              <Hero />
-              <ScrollingLogos />
-              <TechStack />
-              <Features />
-              <CompanyMetrics />
-              <SuccessStories />
-              <Stats />
-              <Testimonials />
-            </>
-          }
-        />
+      {/* Admin Routes - Already Protected */}
+      <Route
+        path="/Adminhomepage"
+        element={
+          <AdminRoute>
+            <AdminHomePage />
+          </AdminRoute>
+        }
+      />
+      <Route
+        path="/Projectmanagement"
+        element={
+          <AdminRoute>
+            <AdminProject />
+          </AdminRoute>
+        }
+      />
+      <Route
+        path="/Admintask"
+        element={
+          <AdminRoute>
+            <AdminTask />
+          </AdminRoute>
+        }
+      />
+      <Route
+        path="/Weeklyreport"
+        element={
+          <AdminRoute>
+            <AdminReport />
+          </AdminRoute>
+        }
+      />
+      <Route
+        path="/Taskassignment"
+        element={
+          <AdminRoute>
+            <AdminTask />
+          </AdminRoute>
+        }
+      />
+      <Route
+        path="/allusers"
+        element={
+          <AdminRoute>
+            <AllUsers />
+          </AdminRoute>
+        }
+      />
+      <Route
+        path="/adminhelppage"
+        element={
+          <AdminRoute>
+            <AdminHelpPage />
+          </AdminRoute>
+        }
+      />
+      <Route
+        path="/intern-attendance"
+        element={
+          <AdminRoute>
+            <InternAttendance />
+          </AdminRoute>
+        }
+      />
+      <Route
+        path="/interntasksubmissions"
+        element={
+          <AdminRoute>
+            <InternTasksSubmissions />
+          </AdminRoute>
+        }
+      />
+      <Route
+        path="view-attendance-all"
+        element={
+          <AdminRoute>
+            <AllAttendance />
+          </AdminRoute>
+        }
+      />
 
-        {/* Public Routes */}
-        <Route path="/login" element={<Signin />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/reset-account-password" element={<ResetPassword />} />
-        <Route path="/aboutus" element={<Aboutus />} />
-        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-        <Route path="/frequently-asked-questions" element={<FAQ />} />
-        <Route path="/help" element={<Help />} />
-        <Route path="*" element={<NotFound />} />
-
-        {/* Admin Routes */}
-        <Route
-          path="/Adminhomepage"
-          element={
-            <AdminRoute>
-              <AdminHomePage />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/admin-projects"
-          element={
-            <AdminRoute>
-              <AdminProject />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/admin-reports"
-          element={
-            <AdminRoute>
-              <AdminReport />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/admin-help"
-          element={
-            <AdminRoute>
-              <AdminHelpPage />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/admin-notify"
-          element={
-            <AdminRoute>
-              <AdminNotify />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/all-users"
-          element={
-            <AdminRoute>
-              <AllUsers />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/leave-application"
-          element={
-            <AdminRoute>
-              <LeaveApplication />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/all-attendance"
-          element={
-            <AdminRoute>
-              <AllAttendance />
-            </AdminRoute>
-          }
-        />
-
-        {/* Protected User Routes */}
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/projects"
-          element={
-            <PrivateRoute>
-              <Projects />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/notifications"
-          element={
-            <PrivateRoute>
-              <Notifications />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <PrivateRoute>
-              <Profile />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/user-attendance"
-          element={
-            <PrivateRoute>
-              <UserAttendance />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/settings"
-          element={
-            <PrivateRoute>
-              <SettingsPage />
-            </PrivateRoute>
-          }
-        />
-
-        {/* Intern Routes */}
-        <Route
-          path="/intern-attendance"
-          element={
-            <PrivateRoute>
-              <InternAttendance />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/intern-tasks"
-          element={
-            <PrivateRoute>
-              <InternTasksSubmissions />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/intern-leave-application"
-          element={
-            <PrivateRoute>
-              <Internleaveapplication />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </div>
+      {/* Protected User Routes */}
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/your-profile"
+        element={
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/notifications"
+        element={
+          <PrivateRoute>
+            <Notifications />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/reports"
+        element={
+          <PrivateRoute>
+            <Reports />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/projects"
+        element={
+          <PrivateRoute>
+            <Projects />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/help"
+        element={
+          <PrivateRoute>
+            <Help />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/my-attendance"
+        element={
+          <PrivateRoute>
+            <UserAttendance />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/stores"
+        element={
+          <PrivateRoute>
+            <Stores />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/leave-application"
+        element={
+          <PrivateRoute>
+            <LeaveApplication />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/setting"
+        element={
+          <PrivateRoute>
+            <SettingsPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/Internleaveapplication"
+        element={
+          <PrivateRoute>
+            <Internleaveapplication />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/logout"
+        element={
+          <PrivateRoute>
+            <Logout />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/notify-all"
+        element={
+          <PrivateRoute>
+            <AdminNotify />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
   );
 };
 

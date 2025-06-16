cb3b5c91b6ec225b6b45a252519ce8053f6fcb05
Yash Midha <72779011+YashMidha@users.noreply.github.com>
2025-06-16 15:09:48 +0530

    Hr intern attendence

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,7 @@ import {
   HrProject,
   HrNotify,
   HrReport,
+  InternAttendancePage,
 } from "./HrHeadAndIntern/HrIndex";
 import AdminHRManagement from "./Admin/AdminHRManagement.jsx";
 import UserManagement from "./Admin/UserManagement.jsx";
@@ -345,6 +346,7 @@ const App = () => {
         <Route path="/hrnotify" element={<HrNotify />} />
         <Route path="/hrreports" element={<HrReport />} />
         <Route path="/hrtaskassignment" element={<HrTaskAssignment />} />
+        <Route path="/hrattendance/:id" element={<InternAttendancePage />} />
 
       </Routes>
     </HrProvider>
